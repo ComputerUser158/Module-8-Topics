3e433c763889228c35a738355cd73ae01da6b5de
ComputerUser158 <oneringraw@gmail.com>
2020-10-20 16:18:34 -0500

    Topic one: first commit, failing tests

---
+++ more_fun_with_collections/set_membership.py
@@ -0,0 +1,10 @@
+"""
+Author: Rawley Collins
+Program: set_membership.py
+
+"""
+
+
+def in_set():
+	pass
+
---
+++ test/test_set_membership.py
@@ -0,0 +1,14 @@
+import unittest
+from more_fun_with_collections import set_membership
+
+
+class MyTestCase(unittest.TestCase):
+	def test_in_set_true(self):
+		self.assertEqual(True, set_membership.in_set(3))
+
+	def test_in_set_false(self):
+		self.assertEqual(False, set_membership.in_set(5))
+
+
+if __name__ == '__main__':
+	unittest.main()
