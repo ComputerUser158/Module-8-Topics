baccfb020665b7325056decdf83fcc7bf7032d92
ComputerUser158 <oneringraw@gmail.com>
2020-10-20 16:37:16 -0500

    Topic one: second commit, passing tests

--- more_fun_with_collections/set_membership.py
+++ more_fun_with_collections/set_membership.py
@@ -5,6 +5,11 @@ Program: set_membership.py
 """
 
 
-def in_set():
-	pass
+def in_set(some_input, some_set):
+	return some_input in some_set
 
+
+if __name__ == '__main__':
+	test_var = 56
+	my_set = set('123467')
+	print("Is {} in {}, {}".format(test_var, my_set, in_set(str(test_var), my_set)))
--- test/test_set_membership.py
+++ test/test_set_membership.py
@@ -4,10 +4,10 @@ from more_fun_with_collections import set_membership
 
 class MyTestCase(unittest.TestCase):
 	def test_in_set_true(self):
-		self.assertEqual(True, set_membership.in_set(3))
+		self.assertEqual(True, set_membership.in_set(str(3), set('123')))
 
 	def test_in_set_false(self):
-		self.assertEqual(False, set_membership.in_set(5))
+		self.assertEqual(False, set_membership.in_set(str(5), set('123')))
 
 
 if __name__ == '__main__':
