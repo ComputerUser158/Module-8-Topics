577bf585bb74d162c57b90296d4779b4c35b4640
ComputerUser158 <oneringraw@gmail.com>
2020-10-20 20:44:24 -0500

    Topic 4 datetime: second commit, test passes

--- more_fun_with_collections/six_months.py
+++ more_fun_with_collections/six_months.py
@@ -7,4 +7,10 @@ from datetime import datetime, timedelta
 
 
 def half_birthday(date):
-	pass
+	new_date = date + timedelta(days=183)  # 183 days because months isn't a thing timedelta can do
+	return new_date
+
+
+if __name__ == '__main__':
+	my_date = datetime(2003, 6, 1)
+	print(half_birthday(my_date))
