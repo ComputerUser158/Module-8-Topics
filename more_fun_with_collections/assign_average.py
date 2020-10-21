cf558edd722e7cfe1713badaddf5813b8f973ba3
ComputerUser158 <oneringraw@gmail.com>
2020-10-20 19:11:35 -0500

    Topic 3 selection dictionary assignment: fifth commit, 2 failing tests

--- more_fun_with_collections/assign_average.py
+++ more_fun_with_collections/assign_average.py
@@ -8,7 +8,8 @@ Program: assign_average.py
 def switch_average(entry):
 	my_dict = {'A': "You entered an A!",
 			   'B': 'You entered an B!',
-			   'C': 'You entered an C!'}
+			   'C': 'You entered an C!',
+			   'D': 'You entered an D!'}
 	return my_dict.get(entry)
 
 
